542a6b8ec361808330e16b01033218f842fa3571
Héctor Matias <hector.matias@newe.eco>
2022-12-22 22:08:33 +0100

    save current money from a business

--- Assets/Scripts/Managers/MoneyManager.cs
+++ Assets/Scripts/Managers/MoneyManager.cs
@@ -1,21 +1,26 @@
 using System;
+using BayatGames.SaveGameFree;
 using UnityEngine;
 
 public class MoneyManager: MonoBehaviour
 {
     public static MoneyManager instance;
-    
-    
-    
     public int CurrentMoney { get; private set; }
+    
+    private string MONEY_KEY = "MY_MONEY";
 
     private void Awake()
     {
         instance = this;
+        if (SaveGame.Exists(MONEY_KEY))
+        {
+            CurrentMoney = SaveGame.Load<int>(MONEY_KEY);
+        }
     }
 
     public void AddMoney(int amount)
     {
         CurrentMoney += amount;
+        SaveGame.Save(MONEY_KEY, CurrentMoney);
     }
 }
